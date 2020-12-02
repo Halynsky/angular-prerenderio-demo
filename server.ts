9675fe3160e515b6379d8985937fcdc272ea9861
Taras Halynskyi <off.bk90@gmail.com>
2020-12-02 09:43:17 +0200

    prerenderio integration

--- server.ts
+++ server.ts
@@ -3,21 +3,23 @@ import * as path from "path";
 
 (async () => {
 
-  const server: express.Application = express();
+  const app: express.Application = express();
 
-  server.use('/', express.static(path.join(__dirname, 'dist')))
+  app.use('/', express.static(path.join(__dirname, 'dist')))
 
   // UI Controller for Angular app
-  server.get(`/my-page-prerendered`, (req, res) => {
+  app.get(`/my-page-prerendered`, (req, res) => {
     res.sendFile('my-page-prerendered.html', { root: path.join(__dirname, 'dist/assets') });
   });
 
-  server.get(/^(?!api).*/, (req, res) => {
+  app.get(/^(?!api).*/, (req, res) => {
     res.sendFile('index.html', { root: path.join(__dirname, 'dist') });
   });
 
+  app.use(require('prerender-node').set('prerenderToken', 'jMWhilZTqX31YvC649bq'));
+
   // run server
-  server.listen(process.env.PORT, () => {
+  app.listen(process.env.PORT, () => {
     console.log("Server is started in " + process.env.NODE_ENV + " mode");
     console.log('Server listening on port ' + process.env.PORT + '!');
   });
